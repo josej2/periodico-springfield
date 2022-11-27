b4dd0fd27158ec5c3ad79c19818d26ca3c94056d
leo <joselmillan1202@gmail.com>
2022-11-26 20:53:25 -0500

    vista de noticias y eliminacion ahora disponible

--- src/routers/autenticacion-columnista.js
+++ src/routers/autenticacion-columnista.js
@@ -38,12 +38,103 @@ rutas.post("/crear-noticia",validarseccion, async(req, res)=>
 
         try{
             await conexionmysql.query('INSERT INTO noticias set ?', [nuevaNoticia])
-            res.send("Noticia Creada")
+            res.redirect('ver-noticias')
         }catch(error ){
             res.send(error)
         }
     }   
 )
+
+
+rutas.get("/eliminar-noticia", validarseccion, (req, res)=>
+    {
+
+        res.render('menu_columnista/eliminar-noticia')
+    }
+)
+
+
+rutas.post("/eliminar-noticia", validarseccion, async (req, res)=>
+    {
+        
+        const dato = await validarToken(req, res).then(token => token);
+
+        const {idbusqueda} = req.body
+
+        const id = parseInt(idbusqueda)
+        console.log(idbusqueda);
+
+        if(dato.rol=="admin"){
+            try{
+                const noticia = await conexionmysql.query('Select id, titulo, descripcion, linkimagen from noticias where id ='+id)
+                  
+                let titulo=""
+                let descripcion=""
+                let linkimagen=""
+                let idnoticia=""
+                
+
+                if(noticia){
+
+                    const objeto = noticia.map(elemento => {
+                        //const objeto = [elemento.titulo, elemento.descripcion ]
+
+                        titulo=elemento.titulo
+                        descripcion = elemento.descripcion
+                        linkimagen = elemento.linkimagen
+                        idnoticia=elemento.id
+                        return  {titulo:elemento.titulo}
+                    })
+
+                    console.log(titulo);
+
+                    res.render('menu_columnista/eliminar-noticia', {titulo, descripcion, linkimagen, idnoticia})
+                }
+                else{
+                    res.send("No hay registro")
+                }
+                
+            }catch(error ){
+                res.send(error)
+            }
+        }
+        else if(dato.rol=="columnista"){
+           res.send("aun no puedes eliminar noticias")
+        }
+    } 
+)
+
+
+
+rutas.post('/eliminacion-total', validarseccion, async (req,res)=>
+    {
+        const dato = await validarToken(req, res).then(token => token);
+        const {id}  = req.body
+
+
+        if(dato.rol=="admin"){
+            try{
+                    await conexionmysql.query('delete from noticias where id ='+id)
+                    res.redirect('ver-noticias')
+                
+            }catch(error ){
+                res.send(error)
+            }
+        }
+        else if(dato.rol=="columnista"){
+            res.send("aun no puede eliminar noticias")
+        }
+    }
+)
+
+
+rutas.get("/editar-noticias", validarseccion, (req, res)=>
+    {
+
+        res.render
+    }
+)
+
 
 //validarseccion
 
